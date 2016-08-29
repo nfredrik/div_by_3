12fcdc35a829dd12cc235e4aeda545bf5abb91e7
Fredrik Svärd <fredrik.svard@gmail.com>
2016-08-29 07:50:13 +0200

    Corrections

--- lib/div_by_3.rb
+++ lib/div_by_3.rb
@@ -1,8 +1,8 @@
 class DivBy_3
-  VERSION = '1.0.1'
+  VERSION = '1.0.2'
 end
 
-def div_with_3(arg)
+def div_by_3(arg)
   raise TypeError unless [Fixnum, Bignum].include?(arg.class)
   (arg.to_s.split('').map(&:to_i).reduce { |a, e| a + e } % 3).zero?
 end
